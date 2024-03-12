3f736c7c8b0b80ce5b2a6a3734c1da59a5f694e8
Uzayisenga <aminauzayisenga@gmail.com>
2024-03-12 13:38:32 +0100

    UpdatedCodes

--- demo/src/test/java/de/frameworktsr/pages/RatingsPage.java
+++ demo/src/test/java/de/frameworktsr/pages/RatingsPage.java
@@ -8,9 +8,6 @@ import io.appium.java_client.AppiumBy;
 public class RatingsPage extends BasePage {
     By ratingScores = AppiumBy.id("io.appium.android.apis:id/indicator_ratingbar");
     public boolean isRatingsScoreUpdated() {
-        // Implement logic to check if the ratings score has been updated
-        // For example, you can find the element representing the ratings score and verify its properties
-        // Return true if the ratings score is updated, false otherwise
         WebElement ratingsScoreElement = getElement(By.id("io.appium.android.apis:id/indicator_ratingbar")); // Replace with actual locator
         String currentScore = ratingsScoreElement.getText(); // Get the current score from the element
         String expectedScore = "5"; // Assuming the expected score is 5
